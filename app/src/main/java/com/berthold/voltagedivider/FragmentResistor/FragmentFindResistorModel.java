d73279d1b714ef413a7f0fcd54c24e557be6cf60
Berthold <berthold.fritz@hotmail.com>
2023-04-03 19:05:58 +0200

    Improved resistor search algorithm

--- app/src/main/java/com/berthold/voltagedivider/FragmentResistor/FragmentFindResistorModel.java
+++ app/src/main/java/com/berthold/voltagedivider/FragmentResistor/FragmentFindResistorModel.java
@@ -11,6 +11,7 @@ import java.util.List;
 
 import VoltageDiv.GetResistors;
 import VoltageDiv.ResistorResult;
+import VoltageDiv.ResistorResults;
 
 public class FragmentFindResistorModel extends ViewModel {
 
@@ -22,15 +23,16 @@ public class FragmentFindResistorModel extends ViewModel {
     //
     // The input
     //
-    public String resistorToBeFoundInput,errorInput;
+    public String resistorToBeFoundInput, errorInput;
 
     //
     // The found solution.
     //
     public MutableLiveData<ProtocolData> resistorValuefoundInAnyOfTheESeries_Ohm;
-    public MutableLiveData<ProtocolData> getResistorValuefoundInAnyOfTheESeries_Ohm(){
-        if(resistorValuefoundInAnyOfTheESeries_Ohm ==null)
-            resistorValuefoundInAnyOfTheESeries_Ohm =new MutableLiveData<ProtocolData>();
+
+    public MutableLiveData<ProtocolData> getResistorValuefoundInAnyOfTheESeries_Ohm() {
+        if (resistorValuefoundInAnyOfTheESeries_Ohm == null)
+            resistorValuefoundInAnyOfTheESeries_Ohm = new MutableLiveData<ProtocolData>();
         return resistorValuefoundInAnyOfTheESeries_Ohm;
     }
 
@@ -40,19 +42,19 @@ public class FragmentFindResistorModel extends ViewModel {
      * @param resistorValue_Ohm
      * @param tolerableErrorIn_P
      */
-    public void findResistor(String resistorValue_Ohm, String tolerableErrorIn_P){
-        try{
-            double r=Double.valueOf(resistorValue_Ohm);
-            double e=Double.valueOf(tolerableErrorIn_P);
+    public void findResistor(String resistorValue_Ohm, String tolerableErrorIn_P) {
+        try {
+            double r = Double.valueOf(resistorValue_Ohm);
+            double e = Double.valueOf(tolerableErrorIn_P);
 
-            List<Integer> exclude=new ArrayList<>();
+            List<Integer> exclude = new ArrayList<>();
 
-            int e3=0;
-            int e6=0;
-            int e12=0;
-            int e24=0;
-            int e48=0;
-            int e96=0;
+            int e3 = 0;
+            int e6 = 0;
+            int e12 = 0;
+            int e24 = 0;
+            int e48 = 0;
+            int e96 = 0;
 
             exclude.add(e3);
             exclude.add(e6);
@@ -60,27 +62,27 @@ public class FragmentFindResistorModel extends ViewModel {
             exclude.add(e24);
             exclude.add(e48);
             exclude.add(e96);
-            List <ResistorResult> listOfResistorsFound=GetResistors.getRValueClosestTo(r,e,exclude);
-            ResistorResult resistorResult=listOfResistorsFound.get(0);
+            ResistorResults listOfResistorsFound = GetResistors.getRValueClosestTo(r, e, exclude);
 
 
-            if (listOfResistorsFound.isEmpty()) {
-                resistorValuefoundInAnyOfTheESeries_Ohm.setValue(new ProtocolData(loc.getNoSolutionFound(),ProtocolData.IS_RESISTOR_RESULT));
+            if (listOfResistorsFound.hasNoSolution()) {
+                resistorValuefoundInAnyOfTheESeries_Ohm.setValue(new ProtocolData(loc.getNoSolutionFound(), ProtocolData.IS_RESISTOR_RESULT));
 
             } else {
                 // toDo: Resistors library, in ResistorResult, add global method to determine max and min resistance
-                double maxR = (1 + resistorResult.getSeriesSpecificErrorMargin() / 100) * resistorResult.getFoundResistorValue_Ohms();
-                double minR = (1 - resistorResult.getSeriesSpecificErrorMargin() / 100) * resistorResult.getFoundResistorValue_Ohms();
+                ResistorResult bestMatchingResistor = listOfResistorsFound.getBestMatchingResistor();
+                double maxR = (1 + bestMatchingResistor.getSeriesSpecificErrorMargin() / 100) * bestMatchingResistor.getFoundResistorValue_Ohms();
+                double minR = (1 - bestMatchingResistor.getSeriesSpecificErrorMargin() / 100) * bestMatchingResistor.getFoundResistorValue_Ohms();
 
-                String solution = loc.getResistorNominalText() + "=" + resistorResult.getFoundResistorValue_Ohms() + "&Omega;" + " E" +resistorResult.getESeries() +
-                        "   (" + resistorResult.getActualError_P() + "%)<hr>" +
-                        loc.geteSeriesErrorMarginText() + " +/-" + resistorResult.getSeriesSpecificErrorMargin() + "%<br>" +
+                String solution = loc.getResistorNominalText() + "=" + bestMatchingResistor.getFoundResistorValue_Ohms() + "&Omega;" + " E" + bestMatchingResistor.getESeries() +
+                        "   (" + bestMatchingResistor.getActualError_P() + "%)<hr>" +
+                        loc.geteSeriesErrorMarginText() + " +/-" + bestMatchingResistor.getSeriesSpecificErrorMargin() + "%<br>" +
                         loc.getResistanceMaxText() + "=" + maxR + " &Omega;<br>" +
                         loc.getResistanceMinText() + "=" + minR + " &Omega;<br>";
 
                 resistorValuefoundInAnyOfTheESeries_Ohm.setValue(new ProtocolData(solution, ProtocolData.IS_RESISTOR_RESULT));
             }
-
-        } catch (NumberFormatException e){}
+        } catch (NumberFormatException e) {
+        }
     }
 }
